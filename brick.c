b79d2c6986fee78668ddfa4e3dc23d946078436e
Alexander Ryakhin <70145449+alexryakhin@users.noreply.github.com>
2022-02-06 11:16:03 +0300

    Create brick.c

---
+++ brick.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    char c = '#';
+    
+    // prints character
+    printf("%c\n", c);
+    
+    // prints character's ascii value
+    printf("%i\n", c);
+}
